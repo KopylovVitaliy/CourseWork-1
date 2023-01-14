3063bf91b7490374595275ab8ef4c027dcb97f4a
PioneerX7 <vita-kopy@yandex.ru>
2023-01-14 15:51:12 +0300

    Промежуточный коммит

---
+++ src/Employee.java
@@ -0,0 +1,65 @@
+import java.util.Objects;
+
+public class Employee {
+    private final String employer;
+    private int department;
+    private double salary;
+
+    public static int counter = 0;
+    private int id;
+
+    public Employee(String employer, int department, double salary) {
+        this.employer = employer;
+        this.department = department;
+        this.salary = salary;
+        id = counter++;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Employee{");
+        sb.append("employer='").append(employer).append('\'');
+        sb.append(", department=").append(department);
+        sb.append(", salary=").append(salary);
+        sb.append(", id=").append(id);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public String getEmployer() {
+        return employer;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && Double.compare(employee.salary, salary) == 0 && id == employee.id && Objects.equals(employer, employee.employer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(employer, department, salary, id);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] employee = new Employee[10];
+
+        employee[0] = new Employee("Астахов Иван Петрович", 1, 23402);
+        employee[1] = new Employee("Ермаков Алексей Иванович", 2, 39281);
+        employee[2] = new Employee("Мартин Полина Валерьевна", 2, 44829);
+        employee[3] = new Employee("Кнауб Владислав Эдуардович", 3, 39520);
+        employee[4] = new Employee("Чухряев Илья Александрович", 4, 45293);
+        employee[5] = new Employee("Сильвестров Владислав Алексеевич", 5, 28736);
+        employee[6] = new Employee("Казакова Алёна Алексеевна", 5, 29873);
+        employee[7] = new Employee("Страхова Алиса Александровна", 4, 57890);
+
     }
 }
